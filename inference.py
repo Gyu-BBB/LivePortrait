5ab4bc4b66db9d2c909621ddedfb35f5f4601c9e
Gyu-BBB <202221014@chu.ac.kr>
2024-10-14 17:40:49 +0900

    update webcame mode

--- inference.py
+++ inference.py
@@ -1,12 +1,6 @@
 # coding: utf-8
-"""
-for human
-"""
 
-import os
-import os.path as osp
 import tyro
-import subprocess
 from src.config.argument_config import ArgumentConfig
 from src.config.inference_config import InferenceConfig
 from src.config.crop_config import CropConfig
@@ -17,36 +11,14 @@ def partial_fields(target_class, kwargs):
     return target_class(**{k: v for k, v in kwargs.items() if hasattr(target_class, k)})
 
 
-def fast_check_ffmpeg():
-    try:
-        subprocess.run(["ffmpeg", "-version"], capture_output=True, check=True)
-        return True
-    except:
-        return False
-
-
-def fast_check_args(args: ArgumentConfig):
-    if not osp.exists(args.source):
-        raise FileNotFoundError(f"source info not found: {args.source}")
-    if not osp.exists(args.driving):
-        raise FileNotFoundError(f"driving info not found: {args.driving}")
-
-
 def main():
     # set tyro theme
     tyro.extras.set_accent_color("bright_cyan")
     args = tyro.cli(ArgumentConfig)
 
-    ffmpeg_dir = os.path.join(os.getcwd(), "ffmpeg")
-    if osp.exists(ffmpeg_dir):
-        os.environ["PATH"] += (os.pathsep + ffmpeg_dir)
-
-    if not fast_check_ffmpeg():
-        raise ImportError(
-            "FFmpeg is not installed. Please install FFmpeg (including ffmpeg and ffprobe) before running this script. https://ffmpeg.org/download.html"
-        )
-
-    fast_check_args(args)
+    # 웹캠 모드 처리
+    if args.use_webcam:
+        args.driving_info = 'webcam'  # 웹캠 모드를 나타내는 특수 값 설정
 
     # specify configs for inference
     inference_cfg = partial_fields(InferenceConfig, args.__dict__)
@@ -61,5 +33,5 @@ def main():
     live_portrait_pipeline.execute(args)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
